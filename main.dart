482eebeffe107822feb3ecedce56f2176e76cc16
LanceDaniel <lancedaniel.g4122@gmail.com>
2024-11-23 21:34:42 +0800

    Add files via upload
    ito lang dinagdag ko sa pubspec.yaml for the logo
    flutter:
        assets:
            - assets/img/

--- main.dart
+++ main.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
-import 'package:testing_flutter/home.dart';
+import 'login_widget.dart';
+import 'register_widget.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: const Home(),
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation Example',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.grey[850],
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: const Color(0xFF1F242D),
       ),
-    ),
-  );
+      initialRoute: 'Login',
+      routes: {
+        'Login': (context) => const LoginWidget(),
+        'Register': (context) => const RegisterWidget(),
+      },
+    );
+  }
 }
